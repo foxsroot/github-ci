bae9f3a4770ed91fa2438a0579f65003beda5ff7
Jer <rickoaxeldinata@gmail.com>
2025-07-31 09:32:36 +0700

    testing

--- src/sayHello.test.ts
+++ src/sayHello.test.ts
@@ -4,4 +4,8 @@ describe("sayHello", () => {
     it("should return a string with the name", () => {
         expect(sayHello("John")).toBe("Hello, John!");
     })
+
+    it("should return a string with the name when called with an empty string", () => {
+        expect(sayHello("a")).toBe("Hello, !");
+    });
 });
